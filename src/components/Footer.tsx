0122e494b581c4ad2508d277abda07fca9d2da8c
panaccospr-arch <230570899+panaccospr-arch@users.noreply.github.com>
2025-10-03 17:57:27 +0000

    style: improve footer and fix hero image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,12 +9,12 @@ export default function Footer() {
       <div className="container mx-auto px-4 flex flex-col sm:flex-row items-center justify-between">
         
         {/* Logo and Copyright */}
-        <div className="flex flex-col items-center sm:items-start mb-6 sm:mb-0">
-          <Image
-            src="/images/panacco-logo.png"
-            alt="Panacco Logo"
-            width={120}
-            height={40}
+         <div className="flex flex-col items-center sm:items-start mb-6 sm:mb-0"> 
+        {/*  <Image */}
+           {/* src="/images/panacco-logo.png" */}
+           {/* alt="Panacco Logo" */}
+           {/* width={120} */}
+           {/* height={40} */}
           />
           <p className="mt-2 text-sm text-gray-500">
             © {new Date().getFullYear()} Panacco. All rights reserved.
