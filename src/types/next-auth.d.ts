2cb2eb4aa7ec65251633c50fffdfabb34a85018e
panaccospr-arch <230570899+panaccospr-arch@users.noreply.github.com>
2025-10-04 16:02:35 +0000

    fix: resolve prisma client sync error

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,6 +1,6 @@
 // src/types/next-auth.d.ts
 
-import NextAuth, { DefaultSession } from "next-auth"
+import { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
   interface Session {
